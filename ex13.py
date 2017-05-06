3203b8310257e56a8d41d7b9bacefb44e47e098b
Marach Treekunprapa <marach.t@gmail.com>
2017-05-06 14:30:16 -0400

    Create ex13.py. - Learn how to import argv module from sys library. - argv is for extracting argument variables. - we do unpacking (line 5) to put input data from argv to variables.

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+input_var = input("Give me some input:")
+
+print("The script is called:", script)
+print("The first is called:", first)
+print("The second is called:", second)
+print("The third is called:", third)
+print("The input_var is:", input_var)
